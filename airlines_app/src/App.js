3221f25b9618501d944fe3ea915f46a6887d8f31
Rohit Parab <rohit@Rohits-MacBook-Air.local>
2018-01-21 00:01:06 +0530

    Changes

--- airlines_app/src/App.js
+++ airlines_app/src/App.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import { Tabs, TabList } from 'react-tabtab';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
+          <h1 className="App-title">Welcome to AirLines Rating</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+       <p className="App-intro">
+
         </p>
+
+        <MuiThemeProvider>
+          <div>
+
+           <TextField
+             hintText="Enter your Username"
+             floatingLabelText="Username"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+
+             <RaisedButton label="Register" primary={true} style={style} onclick={(event) => this.registration(event)}/>
+         </div>
+         </MuiThemeProvider>
       </div>
     );
   }
 }
+
+const style = {
+ margin: 15,
+};
 
 export default App;
